c4ade94ef5f0651c2c5db949cd329dc589a345a8
HM The Mister Techno <giorgos4006@gmail.com>
2019-01-11 00:54:59 +0200

    Add files via upload

---
+++ High Contrast.js
@@ -0,0 +1,55 @@
+﻿// Clear
+function ContrastOff() {
+    var x = document.querySelector('body');
+        x.className = x.className.replace(" contrast", "");
+    
+}
+
+
+function ContrastOn() {
+    var x = document.querySelector('body');
+    if (x.className.indexOf("contrast") == -1) {
+        x.className += " contrast";
+    }
+        x.className = x.className.replace(" YB", "");
+        x.className = x.className.replace(" GB", "");
+        x.className = x.className.replace(" Black", "");
+        x.className = x.className.replace(" White", "");
+}
+
+
+function ContrastA() {
+    var y = document.querySelector('body');
+	ContrastOn();
+    if (y.className.indexOf("YB") == -1) {
+        y.className += " YB";
+    }
+	
+}
+
+function ContrastB() {
+    var y = document.querySelector('body');
+	ContrastOn();
+    if (y.className.indexOf("GB") == -1) {
+        y.className += " GB";
+    }
+	
+}
+
+function ContrastC() {
+    var y = document.querySelector('body');
+	ContrastOn();
+    if (y.className.indexOf("Black") == -1) {
+        y.className += " Black";
+    }
+	
+}
+
+function ContrastD() {
+    var y = document.querySelector('body');
+	ContrastOn();
+    if (y.className.indexOf("White") == -1) {
+        y.className += " White";
+    }
+	
+}
